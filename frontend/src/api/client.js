a586053fb64be2088874251beb790faf566dca82
Anubhaba Swain <anubhabaswain2003@gmail.com>
2026-05-18 22:16:06 +0530

    Update API base URL to production endpoint

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://placement-portal-2epv.onrender.com/api'
 })
 
 client.interceptors.request.use((config) => {
